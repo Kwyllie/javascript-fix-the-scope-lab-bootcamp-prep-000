3d5273256f788d10bb599a42f617ee362abdb1db
nomonkeysforu@gmail.com <nomonkeysforu@gmail.com>
2018-04-02 20:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ function add2(n) {
 var funkyFunction = function() {
    return "FUNKY!"
   return function() {
-    
-  }
- 
+     }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
